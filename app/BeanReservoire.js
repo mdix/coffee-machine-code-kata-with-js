c3be744e0ae471b8191f4c87be3b8c8f878ce05b
mdix <dix@mediaman.de>
2014-07-27 23:14:22 +0200

    Added TODO

--- app/BeanReservoire.js
+++ app/BeanReservoire.js
@@ -3,6 +3,7 @@ var mdix = mdix || {};
 mdix.BeanReservoire = function(beanReservoireConfig) {
     var fillingLevel = 0;
 
+    //TODO: remove x beans depending on what coffee (strong, normal, mild)
 
     this.currentFillingLevel = function() {
         return fillingLevel;
