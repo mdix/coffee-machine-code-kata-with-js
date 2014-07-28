1841a8e4ddc71a055c897a166e2ed7ce53fcb0cd
mdix <dix@mediaman.de>
2014-07-28 11:31:53 +0200

    Checking for the instance of the object on mediatorjs property

--- spec/mediatorSpec.js
+++ spec/mediatorSpec.js
@@ -20,4 +20,8 @@ describe('a mediator', function() {
     it('mediatorjs property should be an instance of mediator.js (global var: Mediator)', function() {
         expect(mediator.mediatorjs instanceof Mediator).toBeTruthy();
     });
+
+    it('mediatorjs property should always be the same instance of mediator.js', function() {
+        expect(mediator.mediatorjs).toEqual(new mdix.Mediator().mediatorjs);
+    });
 });
